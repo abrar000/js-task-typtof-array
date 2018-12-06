35c142b6073ea7b4d3459ac3ad14a6f6dcddc119
abrar000 <abrar@uconnect.pk>
2018-12-06 15:45:14 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,77 @@
+
+
+
+// parametive data types
+
+//string
+let name = "abrar hussain";
+console.log(typeof name);
+
+// number
+let age = 30;
+console.log(typeof age);
+
+// boolean
+let hell = true;
+console.log(typeof hell);
+
+// null
+// object
+let car = null;
+console.log(typeof car);
+
+// undefined
+let test;
+console.log(typeof test);
+
+// symbol
+let sym = Symbol();
+console.log(typeof sym);
+
+// reference type objects
+
+// array
+// object
+let hobbies = ['movies','music','cricket','bike riding'];
+console.log(typeof hobbies);
+console.log(hobbies)
+
+// object literal
+// object
+let address = {
+  city: 'gilgit',
+  country:'pakistan',
+  phoneNumber:'03459600245'
+}
+console.log(address);
+console.log(typeof address);
+
+// date and time
+// object
+let today = new Date();
+console.log(today);
+console.log(typeof today);
+
+
+const personalInfo =[
+  bioData = {
+    name : "abrar",
+    age :25,
+    occupation :"student"
+},
+address = {
+  country :"pakistan",
+  city : "gilgit",
+  village : "sherqilla",
+},
+qualification = {
+  matric :"army public school",
+  fsc :"degree college jutial gilgit",
+  bs :"karakuram international university gilgit",
+},
+];
+console.log(personalInfo);
+console.log(personalInfo[1].city);
+console.log(typeof personalInfo)
+
+
